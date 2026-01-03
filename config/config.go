3a365a9b13d5ff3a050cd5073c0a5bdc5f1246ef
1 <fst8822@gmail.com>
2026-01-03 20:17:41 +0300

    Add config

--- config/config.go
+++ config/config.go
@@ -1,4 +1,37 @@
 package config
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
 type Config struct {
+	HTTPAddress string `envconfig:"HTTP_ADDRESS"`
+	DriverName  string `envconfig:"DRIVER_NAME"`
+	HostDB      string `envconfig:"HOST_DB"`
+	PortDB      string `envconfig:"PORT_DB"`
+	DBUser      string `envconfig:"DB_USER"`
+	DBPassword  string `envconfig:"DB_PASSWORD"`
+	DBName      string `envconfig:"DB_NAME"`
+	SSLMode     string `envconfig:"SSLMODE"`
+}
+
+var (
+	conf Config
+)
+
+func Get() *Config {
+	err := envconfig.Process("", &conf)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	configBytes, err := json.MarshalIndent(conf, "", "    ")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Println("Configuration", string(configBytes))
+	return &conf
 }
---
+++ store/pg/pg.go
@@ -0,0 +1,55 @@
+package pg
+
+import (
+	"erp-2c/config"
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type DB struct {
+	DB *sqlx.DB
+}
+
+func Dial() (*DB, error) {
+	const op = "store.pg.Dial"
+
+	cfg := config.Get()
+	pgURL, err := checkDBFieldsReturnPgUrl(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := sqlx.Connect(cfg.DriverName, pgURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed connect to db %w %s", err, op)
+	}
+	return &DB{db}, nil
+}
+
+func checkDBFieldsReturnPgUrl(cfg *config.Config) (string, error) {
+	const op = "store.pg.checkDBFieldsReturnPgUrl"
+
+	if cfg.DriverName == "" {
+		return "", fmt.Errorf("wrong config field %s, driver name is empty, op=%s", cfg.DriverName, op)
+	}
+	if cfg.DBName == "" {
+		return "", fmt.Errorf("wrong config field %s, driver name is empty, op=%s", cfg.DBName, op)
+	}
+	if cfg.HostDB == "" {
+		return "", fmt.Errorf("wrong config field %s, db name is empty, op=%s", cfg.HostDB, op)
+	}
+	if cfg.PortDB == "" {
+		return "", fmt.Errorf("wrong config field %s, host is empty, op=%s", cfg.PortDB, op)
+	}
+	if cfg.DBUser == "" {
+		return "", fmt.Errorf("wrong config field %s,user name is empty, op=%s", cfg.DBUser, op)
+	}
+	if cfg.DBPassword == "" {
+		return "", fmt.Errorf("wrong config field %s, password name is empty, op=%s", cfg.DBPassword, op)
+	}
+	if cfg.SSLMode == "" {
+		return "", fmt.Errorf("wrong config field %s, sslmode name is empty, op=%s", cfg.SSLMode, op)
+	}
+	return "", nil
+}
