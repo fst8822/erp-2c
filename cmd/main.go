349e5c179f2a5d2291085ade32d48221b64bea78
1 <fst8822@gmail.com>
2026-01-03 20:18:02 +0300

    Add loadENV in main

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"erp-2c/config"
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	loadENV()
+	cfg := config.Get()
+	_ = cfg
+
+}
+
+func loadENV() {
+	env := os.Getenv("ENV")
+	if env == "" {
+		env = "local"
+	}
+	if err := godotenv.Load(".env." + env); err != nil {
+		log.Fatal("No .env file found")
+	}
+	fmt.Printf("RUN APP: env=%s\n", env)
 }
