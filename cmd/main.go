18b50f29bb79804e5803ef1929981b7c61e8bcb8
1 <fst8822@gmail.com>
2026-01-04 09:24:51 +0300

    add chi.router

--- cmd/main.go
+++ cmd/main.go
@@ -5,20 +5,41 @@ import (
 	"erp-2c/store/pg"
 	"fmt"
 	"log"
+	"log/slog"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	loadENV()
 	cfg := config.Get()
-	_ = cfg
 	db, err := pg.Dial()
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 	_ = db
+
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	slog.Info("Start server", slog.String("address", cfg.HTTPAddress))
+	srv := &http.Server{
+		Addr:         cfg.HTTPAddress,
+		Handler:      router,
+		ReadTimeout:  cfg.ReadTimeout,
+		WriteTimeout: cfg.WriteTimeout,
+		IdleTimeout:  cfg.IdleTimeout,
+	}
+	if err := srv.ListenAndServe(); err != nil {
+		slog.Error("Failed to start server", slog.String("error", err.Error()))
+	}
 }
 
 func loadENV() {
