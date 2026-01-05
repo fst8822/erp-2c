8287c7433f351eb53ef2cbe22c3079e111818989
1 <fst8822@gmail.com>
2026-01-05 08:53:30 +0300

    refactor add method check password

--- service/use_cases/auth.go
+++ service/use_cases/auth.go
@@ -3,6 +3,7 @@ package use_cases
 import (
 	"erp-2c/store"
 	"fmt"
+	"log/slog"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -26,7 +27,9 @@ func (a AuthService) SignIn() {
 }
 
 func generatePasswordHash(password string) (string, error) {
-	const op = "service.use"
+	const op = "service.use.generatePasswordHash"
+	slog.Info("Begin get hash password", slog.String("op", op))
+
 	b, err := bcrypt.GenerateFromPassword(
 		[]byte(password),
 		bcrypt.DefaultCost,
@@ -35,4 +38,14 @@ func generatePasswordHash(password string) (string, error) {
 		return "", fmt.Errorf("failed generate password hash %w %s", err, op)
 	}
 	return string(b), err
+}
+func checkPassword(password string, hash string) bool {
+	const op = "service.use.checkPassword"
+	slog.Info("Begin check hash password", slog.String("op", op))
+
+	err := bcrypt.CompareHashAndPassword(
+		[]byte(hash),
+		[]byte(password),
+	)
+	return err == nil
 }
