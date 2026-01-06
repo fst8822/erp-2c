0482b999b19a3a74ecb8936137a770c5457788fb
1 <fst8822@gmail.com>
2026-01-06 22:36:29 +0300

    refactor add arg

--- service/use_cases/user.go
+++ service/use_cases/user.go
@@ -1,6 +1,9 @@
 package use_cases
 
-import "erp-2c/store"
+import (
+	"erp-2c/model"
+	"erp-2c/store"
+)
 
 type UserService struct {
 	store *store.Store
@@ -10,17 +13,17 @@ func NewUserService(store *store.Store) *UserService {
 	return &UserService{store: store}
 }
 
-func (u UserService) Save() {
+func (u *UserService) Save(userToSave model.User) (model.User, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (u UserService) GetById() {
+func (u *UserService) GetById(userId int) (model.User, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (u UserService) GetByName() {
+func (u *UserService) GetByName(userName string) (model.User, error) {
 	//TODO implement me
 	panic("implement me")
 }
--- store/pg/user.go
+++ store/pg/user.go
@@ -1,6 +1,10 @@
 package pg
 
-import "github.com/jmoiron/sqlx"
+import (
+	"erp-2c/model"
+
+	"github.com/jmoiron/sqlx"
+)
 
 type UserRepository struct {
 	db *sqlx.DB
@@ -10,17 +14,17 @@ func NewUserRepository(db *sqlx.DB) *UserRepository {
 	return &UserRepository{db: db}
 }
 
-func (u UserRepository) Save() {
+func (u UserRepository) Save(userToSave model.User) (model.UserDB, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (u UserRepository) GetById() {
+func (u UserRepository) GetById(userId int) (model.UserDB, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (u UserRepository) GetByName() {
+func (u UserRepository) GetByName(userName string) (model.UserDB, error) {
 	//TODO implement me
 	panic("implement me")
 }
