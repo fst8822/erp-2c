0b6bb51c385086e58eeaf61ab3db548589ef46dd
NoutVivo <fst8822@gmail.com>
2026-01-08 21:56:20 +0300

    refactor add migration method

--- store/migrations.go
+++ store/migrations.go
@@ -8,7 +8,9 @@ import (
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jmoiron/sqlx"
+	"log"
 	"log/slog"
+	"os"
 )
 
 func RunPgMigrations(db *sqlx.DB) error {
@@ -25,8 +27,10 @@ func RunPgMigrations(db *sqlx.DB) error {
 	if err != nil {
 		return fmt.Errorf("postgres instance failed %w, op = %s", err, op)
 	}
+	cwd, _ := os.Getwd()
+	log.Println("cwd:", cwd)
 	m, err := migrate.NewWithDatabaseInstance(
-		cfg.PGMigrationsPath, "postgres", driver)
+		"file://./store/pg/migrations", "postgres", driver)
 	if err != nil {
 		return err
 	}
