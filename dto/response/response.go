64548f4150ab3da8944fcc28bc8852bbf11ad5b4
1 <fst8822@gmail.com>
2026-01-08 09:57:26 +0300

    response

--- dto/response/response.go
+++ dto/response/response.go
@@ -1,13 +1,70 @@
 package response
 
-type ErrResponse struct {
+const (
+	statusOK                  = 200
+	statusCreated             = 201
+	statusBadRequest          = 400
+	statusUnauthorized        = 401
+	statusForbidden           = 403
+	statusNotFound            = 404
+	statusConflict            = 409
+	statusInternalServerError = 500
+)
+
+type Response struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
+	Body    any    `json:"body,omitempty"`
 }
 
-func Error(code int, err error) ErrResponse {
-	return ErrResponse{
-		Code:    code,
-		Message: err.Error(),
-	}
+func New(code int, message string, body any) *Response {
+	return &Response{Code: code, Message: message, Body: body}
+}
+
+func Success(code int, msg string, body any) *Response {
+	return New(code, msg, body)
+}
+
+func Error(code int, msg string) *Response {
+	return New(code, msg, nil)
+}
+
+func OK(msg string, body any) *Response {
+	return New(statusOK, msg, body)
+}
+
+func Created(msg string, body any) *Response {
+	return New(statusCreated, msg, body)
+}
+
+func BadRequest(message string, body any) *Response {
+	return New(statusBadRequest, message, body)
+}
+
+func Unauthorized(message string) *Response {
+	return New(statusUnauthorized, message, nil)
+}
+
+func Forbidden(message string) *Response {
+	return New(statusForbidden, message, nil)
+}
+
+func NotFound(message string) *Response {
+	return New(statusNotFound, message, nil)
+}
+
+func AlreadyExist(message string) *Response {
+	return New(statusConflict, message, nil)
+}
+
+func InternalServerError(message string) *Response {
+	return New(statusInternalServerError, message, nil)
+}
+
+func ErrorWithData(code int, message string, data any) *Response {
+	return New(code, message, data)
+}
+
+func (r *Response) WithBody(body any) {
+	r.Body = body
 }
