ab7d33f86c8ed0b3b33259f7665d522bed66452f
1 <fst8822@gmail.com>
2026-01-08 09:57:51 +0300

    services.go

--- service/services.go
+++ service/services.go
@@ -5,20 +5,20 @@ import (
 )
 
 type AuthService interface {
-	SignUp(UserToSave model.User) (string, error)
+	SignUp(UserToSave model.User) (*model.User, error)
 	SignIn(login string, password string) (string, error)
 }
 
 type UserService interface {
-	Save(userToSave model.User) (model.User, error)
-	GetById(userId int) (model.User, error)
-	GetByName(userName string) (model.User, error)
+	Save(userToSave model.User) (*model.User, error)
+	GetById(userId int) (*model.User, error)
+	GetByName(userName string) (*model.User, error)
 }
 type ProductService interface {
-	Save(productToSave model.Product) (model.Product, error)
-	GetById(productId int) (model.Product, error)
-	GetByName(productName string) (model.Product, error)
-	GetAll() ([]model.Product, error)
+	Save(productToSave model.Product) (*model.Product, error)
+	GetById(productId int) (*model.Product, error)
+	GetByName(productName string) (*model.Product, error)
+	GetAll() (*[]model.Product, error)
 	UpdateById(productId int) error
 	DeleteById(productId int) error
 }
