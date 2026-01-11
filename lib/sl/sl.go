2bf0432d4e94a0b5a07f64a5def080383626b959
1 <fst8822@gmail.com>
2026-01-11 20:00:59 +0300

    refactor add att OP

--- lib/sl/sl.go
+++ lib/sl/sl.go
@@ -1,6 +1,17 @@
 package sl
 
-import "golang.org/x/exp/slog"
+import (
+	"fmt"
+
+	"golang.org/x/exp/slog"
+)
+
+func ErrWithOP(err error, op string) slog.Attr {
+	return slog.Attr{
+		Key:   "error",
+		Value: slog.StringValue(fmt.Sprintf(err.Error(), op)),
+	}
+}
 
 func Err(err error) slog.Attr {
 	return slog.Attr{
