36126d6484fb0bbc5a78c9038a0f55ed9a8f3505
1 <fst8822@gmail.com>
2026-01-11 20:00:01 +0300

    fix bug repo

--- store/repositories.go
+++ store/repositories.go
@@ -4,15 +4,16 @@ import "erp-2c/model"
 
 type ProductRepository interface {
 	Save(productToSave model.ProductDB) (*model.ProductDB, error)
-	GetById(productId int) (*model.ProductDB, error)
+	GetById(productId int64) (*model.ProductDB, error)
 	GetByName(productName string) (*model.ProductDB, error)
 	GetAll() ([]model.ProductDB, error)
-	UpdateById(productId int, productToUpdate model.ProductDB) error
-	DeleteById(productId int) error
+	UpdateById(productId int64, productToUpdate model.ProductDB) error
+	DeleteById(productId int64) error
 	GetByGroupName(groupId string) ([]model.ProductDB, error)
 }
 
 type UserRepository interface {
 	Save(userToSave model.UserDB) (*model.UserDB, error)
-	GetById(userId int) (*model.UserDB, error)
+	GetById(userId int64) (*model.UserDB, error)
+	GetByLogin(userId string) (*model.UserDB, error)
 }
