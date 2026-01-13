5eeeb923d3a7b5a8b7296eb6d41d95ea33d13e2a
1 <fst8822@gmail.com>
2026-01-14 01:13:31 +0300

    delete ErrInternalServerError

--- lib/types/errs.go
+++ lib/types/errs.go
@@ -3,9 +3,10 @@ package types
 import "errors"
 
 var (
-	ErrNotFound     = errors.New("resource not found")
-	ErrAlreadyExist = errors.New("entity already exist")
-	ErrForbidden    = errors.New("forbidden access")
-	ErrBadRequest   = errors.New("bad request")
-	ErrUnauthorized = errors.New("unauthorized")
+	ErrNotFound            = errors.New("resource not found")
+	ErrAlreadyExist        = errors.New("entity already exist")
+	ErrForbidden           = errors.New("forbidden access")
+	ErrBadRequest          = errors.New("bad request")
+	ErrUnauthorized        = errors.New("unauthorized")
+	ErrInternalServerError = errors.New("internalServerError")
 )
