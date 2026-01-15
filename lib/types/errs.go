4634ef54cfc810df244f475adbb608141111e02e
1 <fst8822@gmail.com>
2026-01-15 10:32:57 +0300

    add handle  err

--- lib/types/errs.go
+++ lib/types/errs.go
@@ -3,10 +3,29 @@ package types
 import "errors"
 
 var (
-	ErrNotFound            = errors.New("resource not found")
-	ErrAlreadyExist        = errors.New("entity already exist")
-	ErrForbidden           = errors.New("forbidden access")
-	ErrBadRequest          = errors.New("bad request")
-	ErrUnauthorized        = errors.New("unauthorized")
-	ErrInternalServerError = errors.New("internalServerError")
+	ErrNotFound        = errors.New("resource not found")
+	ErrAlreadyExist    = errors.New("entity already exist")
+	ErrForbidden       = errors.New("forbidden access")
+	ErrBadRequest      = errors.New("bad request")
+	ErrUnauthorized    = errors.New("unauthorized")
+	ErrInternalServer  = errors.New("internalServerError")
+	ErrDatabaseTimeout = errors.New("database timeout")
+	ErrNoFieldsUpdate  = errors.New("no fields to update")
 )
+
+type AppErr struct {
+	Message string
+	Err     error
+}
+
+func NewAppErr(message string, err error) *AppErr {
+	return &AppErr{Message: message, Err: err}
+}
+
+func (a *AppErr) Error() string {
+	return a.Err.Error()
+}
+
+func (a *AppErr) Unwrap() error {
+	return a.Err
+}
