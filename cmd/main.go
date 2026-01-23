eb4c90ca574ac54bd1cc5f81d3fd4ecff0eb2a62
1 <fst8822@gmail.com>
2026-01-23 11:19:42 +0300

    add method gracefully

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"erp-2c/config"
 	"erp-2c/controller/routers"
 	"erp-2c/service/use_cases"
@@ -12,12 +13,16 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	if err := run(); err != nil {
+	ctx := context.Background()
+	if err := run(ctx); err != nil {
 		log.Fatal(err)
 	}
 }
@@ -33,7 +38,8 @@ func loadENV() {
 	fmt.Printf("RUN APP: env=%s\n", env)
 }
 
-func run() error {
+func run(ctx context.Context) error {
+
 	loadENV()
 
 	cfg := config.Get()
@@ -57,7 +63,6 @@ func run() error {
 
 	r := routers.New(serviceManager)
 
-	slog.Info("Start server", slog.String("address", cfg.HTTPAddress))
 	srv := &http.Server{
 		Addr:         cfg.HTTPAddress,
 		Handler:      r,
@@ -65,10 +70,37 @@ func run() error {
 		WriteTimeout: cfg.WriteTimeout,
 		IdleTimeout:  cfg.IdleTimeout,
 	}
-	//todo add method for grace full shutdown
-	if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-		slog.Error("Failed to start server", slog.String("error", err.Error()))
+	serverErrCh := make(chan error)
+	go func() {
+		slog.Info("Start server", slog.String("address", cfg.HTTPAddress))
+
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("Failed to start server", slog.String("error", err.Error()))
+			serverErrCh <- err
+		}
+		close(serverErrCh)
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err = <-serverErrCh:
+		return err
+	case <-stop:
+		slog.Info("Shutdown signal is received")
+	case <-ctx.Done():
+		slog.Info("Context cancelled")
+	}
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		if closeErr := srv.Close(); closeErr != nil {
+			return errors.Join(err, closeErr)
+		}
 		return err
 	}
+	slog.Info("Server shutdown gracefully")
 	return nil
 }
