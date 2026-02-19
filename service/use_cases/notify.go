37610aa53e372f0fd319dd82ade8534dc6294338
1 <fst8822@gmail.com>
2026-02-19 20:08:29 +0300

    add ch done

--- service/use_cases/notify.go
+++ service/use_cases/notify.go
@@ -16,53 +16,62 @@ const PingInterval = 10 * time.Second
 type NotifyService struct {
 	mu      sync.RWMutex
 	clients map[int64]*model.ClientWS
+	done    chan struct{}
+	wg      sync.WaitGroup
 }
 
 func NewNotifyService() *NotifyService {
 	return &NotifyService{
 		clients: make(map[int64]*model.ClientWS),
+		done:    make(chan struct{}),
 	}
 }
 
 func (n *NotifyService) Subscribe(ctx context.Context, client *model.ClientWS) {
-	const OP = "services.use_cases.notify.NewNotifyService.Subscribe"
+	const OP = "services.use_cases.notify.NotifyService.Subscribe"
 	log := slog.With("OP", OP, "UserID", client.UserID)
 
-	defer func() {
-		n.RemoveClient(client)
+	n.wg.Add(1)
+	go func() {
+		defer n.wg.Done()
+		defer func() {
+			n.RemoveClient(client)
+		}()
+
+		for {
+			log.Info("Start WS worker")
+			select {
+			case <-ctx.Done():
+				log.Info("Client disconnected, context is don, exit from subscribe")
+				client.Conn.WriteMessage(websocket.CloseMessage,
+					websocket.FormatCloseMessage(websocket.CloseNormalClosure, "Client disconnected"))
+				return
+
+			case <-n.done:
+				log.Info("Server is shutdown, closing connection")
+				client.Conn.WriteMessage(websocket.CloseMessage,
+					websocket.FormatCloseMessage(websocket.CloseNormalClosure, "Server is shutdown"))
+				return
+
+			case notification, ok := <-client.Cn:
+				if !ok {
+					log.Info("Notification channel is close, exit from subscribe")
+					return
+				}
+				if err := client.Conn.WriteJSON(notification); err != nil {
+					log.Error("websocket is close, exit from subscribe", sl.Err(err))
+					return
+				}
+				slog.Info("Send message successful", slog.Any("notification", notification))
+			}
+			log.Info("End WS worker")
+		}
 	}()
 
-	ticket := time.NewTicker(PingInterval)
-	defer ticket.Stop()
-
-	for {
-		select {
-		case <-ctx.Done():
-			log.Info("Context is don, exit from subscribe")
-			client.Conn.WriteMessage(websocket.CloseMessage,
-				websocket.FormatCloseMessage(websocket.CloseNormalClosure, "context is done"))
-			return
-		case notification, ok := <-client.Cn:
-			if !ok {
-				log.Info("Channel is close, exit from subscribe")
-				return
-			}
-			if err := client.Conn.WriteJSON(notification); err != nil {
-				log.Error("websocket is close, exit from subscribe", sl.Err(err))
-				return
-			}
-			slog.Info("Send message successful", slog.Any("notification", notification))
-		case <-ticket.C:
-			if err := client.Conn.WriteMessage(websocket.PingMessage, nil); err != nil {
-				log.Error("websocket is close, exit from subscribe", sl.Err(err))
-				return
-			}
-		}
-	}
 }
 
 func (n *NotifyService) SendNotify(notification model.Notification) {
-	const OP = "services.use_cases.notify.NewNotifyService.SendNotify"
+	const OP = "services.use_cases.notify.NotifyService.SendNotify"
 	log := slog.With("OP", OP, "UserID", notification.UserID)
 
 	n.mu.RLock()
@@ -86,24 +95,32 @@ func (n *NotifyService) SendNotify(notification model.Notification) {
 func (n *NotifyService) AddClient(client *model.ClientWS) {
 	n.mu.Lock()
 	defer n.mu.Unlock()
+	if old, exist := n.clients[client.UserID]; exist {
+		old.Conn.Close()
+		close(old.Cn)
+		delete(n.clients, old.UserID)
+	}
 	n.clients[client.UserID] = client
+
 }
 
 func (n *NotifyService) RemoveClient(client *model.ClientWS) {
 	n.mu.Lock()
 	defer n.mu.Unlock()
 
-	found, ok := n.clients[client.UserID]
-	if !ok || found.UUID != client.UUID {
-		client.Conn.Close()
-		close(client.Cn)
-		return
+	if found, ok := n.clients[client.UserID]; ok && found == client {
+		found.Conn.Close()
+		close(found.Cn)
+		delete(n.clients, found.UserID)
 	}
-	found.Conn.Close()
-	delete(n.clients, found.UserID)
-	close(found.Cn)
 }
 
-func (n *NotifyService) Shutdown(ctx context.Context) {
+func (n *NotifyService) Shutdown() {
+	const OP = "services.use_cases.notify.NotifyService.Shutdown"
+	log := slog.With("OP", OP)
 
+	log.Info("Start shutdown is client ws")
+	close(n.done)
+	n.wg.Wait()
+	log.Info("Shutdown client ws is done")
 }
