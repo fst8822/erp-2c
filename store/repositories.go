c40b67a9cff81de98c251bbd2e55cb00a3ec8947
1 <fst8822@gmail.com>
2026-03-04 13:27:11 +0300

    refactor method repositories.go add GetStatusCount

--- store/repositories.go
+++ store/repositories.go
@@ -25,7 +25,7 @@ type UserRepository interface {
 
 type DeliveryRepository interface {
 	SaveWithItems(tx *sqlx.Tx, deliveryWithItems model.DeliveryWithItemsDB) (*model.DeliveryDB, error)
-	GetWithItemsById(tx *sqlx.Tx, deliveryId int64) (*model.DeliveryWithItemsDB, error)
+	GetWithItemsById(tx *sqlx.Tx, deliveryId int64) (model.DeliveryWithItemsDB, error)
 	GetAll(tx *sqlx.Tx) (*model.DeliverListDB, error)
 	GetAllWithItemsByStatus(tx *sqlx.Tx, status model.DeliveryStatus) (*model.DeliverListDB, error)
 	LockAndGetDeliveries(status model.DeliveryStatus) ([]model.DeliveryDB, error)
@@ -33,4 +33,5 @@ type DeliveryRepository interface {
 	DeleteById(tx *sqlx.Tx, deliveryId int64) error
 	UpdateStatusById(tx *sqlx.Tx, id int64, status model.DeliveryStatus) error
 	UpdateStatusByIds(tx *sqlx.Tx, groups map[model.DeliveryStatus][]int64) error
+	GetStatusCount(tx *sqlx.Tx) ([]model.StatusCount, error)
 }
