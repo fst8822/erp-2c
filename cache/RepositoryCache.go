f2123bb4a86d1f82bc222662a29461160a13ce90
1 <fst8822@gmail.com>
2026-03-04 13:23:50 +0300

    refactor cache

--- cache/RepositoryCache.go
+++ cache/RepositoryCache.go
@@ -3,25 +3,89 @@ package cache
 import (
 	"context"
 	"erp-2c/model"
+	"erp-2c/store"
+
+	"github.com/jmoiron/sqlx"
 )
 
 type RepositoryCache struct {
-	MapCache Cache
+	store    store.Store
+	mapCache Cache
+	done     chan struct{}
 }
 
-func NewRepositoryCache(mapCache Cache) *RepositoryCache {
-	mapCache.cleanTTL()
-	return &RepositoryCache{MapCache: mapCache}
+func NewRepositoryCache(
+	ctx context.Context,
+	store *store.Store,
+	mapCache Cache,
+	done chan struct{}) *RepositoryCache {
+	repo := RepositoryCache{store: *store, mapCache: mapCache, done: done}
+
+	go mapCache.cleanTTL(ctx, repo.done)
+	return &repo
 }
 
-func (c *RepositoryCache) GetAll(ctx context.Context) []model.DeliveryDB {
-	return make([]model.DeliveryDB, 0)
+func (c *RepositoryCache) GetAll(ctx context.Context, tx *sqlx.Tx) (*model.DeliverListDB, error) {
+	deliveries := make([]model.DeliveryDB, 100)
+	itemsDB := make([]model.ItemsDB, 100)
+
+	if vAny, ok := c.mapCache.GetAll(); ok {
+		for _, v := range vAny {
+			if itemCaches, ok := v.(itemCache); ok {
+				if cached, ok := itemCaches.any.(model.DeliveryWithItemsDB); ok {
+					deliveries = append(deliveries, cached.DeliveryDB)
+					itemsDB = append(itemsDB, cached.DeliveryItemsDB...)
+				}
+			}
+		}
+
+		return &model.DeliverListDB{
+			DeliveriesDB: deliveries,
+			ItemsDB:      itemsDB,
+		}, nil
+	}
+
+	deliverListDB, err := c.store.Delivery.GetAll(tx)
+	if err != nil {
+		return nil, err
+	}
+	c.addList(deliverListDB)
+
+	return deliverListDB, nil
 }
 
-func (c *RepositoryCache) Get(ctx context.Context) model.DeliveryDB {
-	return model.DeliveryDB{}
+func (c *RepositoryCache) GetById(ctx context.Context, tx *sqlx.Tx, id int64) (model.DeliveryWithItemsDB, error) {
+	if v, ok := c.mapCache.Get(id); ok {
+		if v2, ok2 := v.(itemCache); ok2 {
+			if cached, ok3 := v2.any.(model.DeliveryWithItemsDB); ok3 {
+				return cached, nil
+			}
+		}
+	}
+	deliveryWithItemsDB, err := c.store.Delivery.GetWithItemsById(tx, id)
+	if err != nil {
+		return model.DeliveryWithItemsDB{}, err
+	}
+	c.Add(deliveryWithItemsDB)
+
+	return deliveryWithItemsDB, nil
 }
 
-func (c *RepositoryCache) Add(ctx context.Context) {
+func (c *RepositoryCache) Add(delivery model.DeliveryWithItemsDB) {
+	c.mapCache.Add(delivery)
+}
 
+func (c *RepositoryCache) addList(deliverListDB *model.DeliverListDB) {
+	mapItem := make(map[int64][]model.ItemsDB)
+	for _, v := range deliverListDB.ItemsDB {
+		mapItem[v.DeliveryID] = append(mapItem[v.DeliveryID], v)
+	}
+
+	for _, v := range deliverListDB.DeliveriesDB {
+		deliveryWithItemsDB := model.DeliveryWithItemsDB{
+			DeliveryDB:      v,
+			DeliveryItemsDB: mapItem[v.ID],
+		}
+		c.Add(deliveryWithItemsDB)
+	}
 }
--- cache/cache.go
+++ cache/cache.go
@@ -1,15 +1,18 @@
 package cache
 
 import (
+	"context"
 	"erp-2c/model"
+	"log/slog"
 	"sync"
 	"time"
 )
 
 type Cache interface {
-	Get(delivery model.DeliveryDB) (any, bool)
-	Add(delivery model.DeliveryDB)
-	cleanTTL()
+	Get(id int64) (any, bool)
+	GetAll() ([]any, bool)
+	Add(delivery model.DeliveryWithItemsDB)
+	cleanTTL(ctx context.Context, done chan struct{})
 }
 
 type itemCache struct {
@@ -26,25 +29,53 @@ func NewMapCache(TTL time.Duration) *MapCache {
 	return &MapCache{tTL: TTL}
 }
 
-func (c *MapCache) Get(delivery model.DeliveryDB) (any, bool) {
-	return c.data.Load(delivery.ID)
-
+func (c *MapCache) GetAll() ([]any, bool) {
+	var res []any
+	c.data.Range(func(key any, value any) bool {
+		res = append(res, value)
+		return true
+	})
+	if len(res) == 0 {
+		return res, true
+	}
+	return nil, false
 }
 
-func (c *MapCache) Add(delivery model.DeliveryDB) {
+func (c *MapCache) Get(id int64) (any, bool) {
+	return c.data.Load(id)
+}
 
-	c.data.Store(delivery.ID, itemCache{
-		any:       delivery,
+func (c *MapCache) Add(deliveryWithItems model.DeliveryWithItemsDB) {
+	c.data.Store(deliveryWithItems.DeliveryDB.ID, itemCache{
+		any:       deliveryWithItems,
 		ExpiresAt: time.Now().Add(c.tTL),
 	})
 }
 
-func (c *MapCache) cleanTTL() {
-	c.data.Range(func(key, value any) bool {
-		item := value.(itemCache)
-		if time.Now().After(item.ExpiresAt) {
-			c.data.Delete(key)
-		}
-		return true
-	})
+func (c *MapCache) cleanTTL(ctx context.Context, done chan struct{}) {
+	const op = "cache.cleanTTL"
+	logger := slog.With("op", op)
+
+	ticket := time.NewTicker(c.tTL)
+	defer ticket.Stop()
+
+	select {
+	case <-ctx.Done():
+		logger.Info("Stop clear ttl, context is done")
+		return
+	case <-done:
+		logger.Info("Stop clear ttl, receive signal done")
+		return
+	case <-ticket.C:
+		logger.Info("Start clear ttl")
+		c.data.Range(func(key, value any) bool {
+			item := value.(itemCache)
+			if time.Now().After(item.ExpiresAt) {
+				c.data.Delete(key)
+			}
+			return true
+		})
+	default:
+		logger.Info("skip clear iteration")
+	}
 }
