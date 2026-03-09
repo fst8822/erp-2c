94b5629b336f32d30a164e525548d63e37ac9f35
1 <fst8822@gmail.com>
2026-03-09 06:29:56 +0300

    refact cache

---
+++ store/pg/delivery_cache.go
@@ -0,0 +1,133 @@
+package pg
+
+import (
+	"context"
+	"erp-2c/model"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type cacheInt interface {
+	Get(key int64) (any, bool)
+	GetAll() ([]any, bool)
+	Add(key int64, value any)
+	CleanTTL(ctx context.Context)
+	DeleteByKey(key int64)
+	DeleteByKeys(keys []int64)
+}
+
+type DeliveryCache struct {
+	deliveryRepo *DeliveryRepository
+	cache        cacheInt
+}
+
+func NewDeliveryCacheCache(ctx context.Context, deliveryRepo *DeliveryRepository, cache cacheInt,
+) *DeliveryCache {
+	repo := DeliveryCache{deliveryRepo: deliveryRepo, cache: cache}
+	go cache.CleanTTL(ctx)
+	return &repo
+}
+
+func (c *DeliveryCache) SaveWithItems(
+	tx *sqlx.Tx, deliveryWithItems model.DeliveryWithItemsDB) (*model.DeliveryDB, error) {
+
+	DeliveryDB, err := c.deliveryRepo.SaveWithItems(tx, deliveryWithItems)
+	if err != nil {
+		return nil, err
+	}
+	deliveryWithItems.DeliveryDB.ID = DeliveryDB.ID
+	c.cache.Add(DeliveryDB.ID, deliveryWithItems)
+	return DeliveryDB, nil
+}
+
+func (c *DeliveryCache) GetWithItemsById(tx *sqlx.Tx, deliveryId int64) (model.DeliveryWithItemsDB, error) {
+	if v, ok := c.cache.Get(deliveryId); ok {
+		if deliveryItems, ok2 := v.(model.DeliveryWithItemsDB); ok2 {
+			return deliveryItems, nil
+		}
+	}
+	deliveryWithItemsDB, err := c.deliveryRepo.GetWithItemsById(tx, deliveryId)
+	if err != nil {
+		return model.DeliveryWithItemsDB{}, err
+	}
+	c.cache.Add(deliveryId, deliveryWithItemsDB)
+
+	return deliveryWithItemsDB, nil
+}
+
+func (c *DeliveryCache) GetAll(tx *sqlx.Tx) (*model.DeliverListDB, error) {
+	deliveries := make([]model.DeliveryDB, 100)
+	itemsDB := make([]model.ItemsDB, 100)
+	isInterrupt := false
+
+	if all, ok := c.cache.GetAll(); ok {
+		for _, v := range all {
+			it, ok2 := v.(model.DeliveryWithItemsDB)
+			if !ok2 {
+				isInterrupt = true
+				break
+			}
+			deliveries = append(deliveries, it.DeliveryDB)
+			itemsDB = append(itemsDB, it.DeliveryItemsDB...)
+		}
+		if !isInterrupt {
+			return &model.DeliverListDB{
+				DeliveriesDB: deliveries,
+				ItemsDB:      itemsDB,
+			}, nil
+		}
+	}
+
+	deliverListDB, err := c.deliveryRepo.GetAll(tx)
+	if err != nil {
+		return nil, err
+	}
+	c.addList(deliverListDB)
+
+	return deliverListDB, nil
+}
+
+func (c *DeliveryCache) GetAllWithItemsByStatus(tx *sqlx.Tx, status model.DeliveryStatus) (*model.DeliverListDB, error) {
+	return c.deliveryRepo.GetAllWithItemsByStatus(tx, status)
+}
+
+func (c *DeliveryCache) UpdateById(tx *sqlx.Tx, deliveryId int64, status model.UpdateStatus) error {
+	c.cache.DeleteByKey(deliveryId)
+	return c.deliveryRepo.UpdateById(tx, deliveryId, status)
+}
+
+func (c *DeliveryCache) DeleteById(tx *sqlx.Tx, deliveryId int64) error {
+	c.cache.DeleteByKey(deliveryId)
+	return c.deliveryRepo.DeleteById(tx, deliveryId)
+}
+
+func (c *DeliveryCache) UpdateStatusById(tx *sqlx.Tx, deliveryId int64, status model.DeliveryStatus) error {
+	c.cache.DeleteByKey(deliveryId)
+	return c.deliveryRepo.UpdateStatusById(tx, deliveryId, status)
+}
+
+func (c *DeliveryCache) UpdateStatusByIds(tx *sqlx.Tx, groups map[model.DeliveryStatus][]int64) error {
+	for _, keys := range groups {
+		c.cache.DeleteByKeys(keys)
+	}
+	return c.deliveryRepo.UpdateStatusByIds(tx, groups)
+}
+
+func (c *DeliveryCache) BeginTxx(ctx context.Context) (*sqlx.Tx, error) {
+	return c.deliveryRepo.BeginTxx(ctx)
+}
+
+func (c *DeliveryCache) addList(deliverListDB *model.DeliverListDB) {
+	mapItem := make(map[int64][]model.ItemsDB)
+	for _, v := range deliverListDB.ItemsDB {
+		mapItem[v.DeliveryID] = append(mapItem[v.DeliveryID], v)
+	}
+
+	for _, v := range deliverListDB.DeliveriesDB {
+		deliveryWithItemsDB := model.DeliveryWithItemsDB{
+			DeliveryDB:      v,
+			DeliveryItemsDB: mapItem[v.ID],
+		}
+		c.cache.Add(v.ID, deliveryWithItemsDB)
+	}
+}
